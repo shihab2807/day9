50a4cba0b302d01363ed75f97efd8513b09192a1
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-22 07:53:50 +0100

    day9 done

---
+++ day9/src/day9/demo3.java
@@ -0,0 +1,22 @@
+package day9;
+
+public class demo3 {
+
+	public static void main(String[] args) {
+		String s = "friends";
+
+		int x = 0;
+
+		do{
+
+		System.out.print(s.charAt(x));
+
+		x++ ;
+
+		}while (x < 2);
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
