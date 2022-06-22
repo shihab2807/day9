80e2d9951ea9fce72cd08bb1050edc64e753c3c7
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-22 08:05:27 +0100

    day9 done

---
+++ day9/src/day9/demo8.java
@@ -0,0 +1,25 @@
+package day9;
+
+public class demo8 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i<= 2; i++) {
+
+			for (int j = i; j<=5; j++) {
+
+			if(j == 4) { //4==4
+
+			continue; //forecful iteration
+
+			}
+
+			System.out.println(j); //01235 1235 235
+
+			}
+
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+}
