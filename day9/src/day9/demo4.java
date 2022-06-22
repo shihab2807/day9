baab12d5806a90f795d99783bb72ef2498a7e1dd
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-22 07:56:01 +0100

    day9 done

---
+++ day9/src/day9/demo4.java
@@ -0,0 +1,29 @@
+package day9;
+
+public class demo4 {
+
+	public static void main(String[] args) {
+		{
+
+			for(int i = 1; i <= 10; i++)
+
+			{
+
+			System.out.println("before breaking = " + i );
+
+			if(i == 6)
+
+			break;
+
+			System.out.println("after breaking = " + i );
+
+			}
+
+			System.out.println("for loop terminated");
+
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
