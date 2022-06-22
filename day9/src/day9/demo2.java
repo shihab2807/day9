324b35bcd2a0a43f62e7075b75166ce2cf2289c0
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-22 07:52:02 +0100

    day9 done

---
+++ day9/src/day9/demo2.java
@@ -0,0 +1,19 @@
+package day9;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i++)
+
+		{
+
+		System.out.println("Hello");
+
+		i++;
+
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
