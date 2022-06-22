2c2bd723d93f7349767ca24982f5a87b8a9e322c
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-22 08:10:55 +0100

    day9 done

---
+++ day9/src/day9/demo10.java
@@ -0,0 +1,27 @@
+package day9;
+
+public class demo10 {
+
+	public static void main(String[] args) {
+		int i = 1;
+
+		while(i<=10){ //1<=10 6<=10
+
+		if(i==5){ //5==5
+
+		i++; //6
+
+		continue;
+
+		}
+
+		System.out.println(i); //1 2 3 4 6...10
+
+		i++;//2 3 5
+
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
